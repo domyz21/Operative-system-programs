b95192929eabb2cc7813bd4ffc0d1fc1728357c4
Domy <158062441+domyz21@users.noreply.github.com>
2024-02-06 23:22:21 +0100

    Update fork.c

--- fork.c
+++ fork.c
@@ -11,10 +11,14 @@ int main(void) {
     char arg[] = {"ls", "/", NULL}; //argomenti per l'execve
 
     pid = fork(); //crea una copia del processo padre
-
+    
     if(pid > 0){ //siamo nel padre
         waitpid(pid, &status , 0); //pid del figlio ceh dobbiamo aspettare, status dell'exit
         printf("%d", WEXITSTATUS(status)); //stampa lo stato dell'exit, 0 se non ci sono problemi
+        
+        //stampi il valore di pid (fork) se vuoi stampare il pid del parde
+        //getpid() per ottenere il pid (del padre)
+        
     }else if(pid == 0){ //siamo nel figlio
         sprintf(buff, "Hello\n");
         write(STDOUT_FILENO, buff, strlen(buff)); //macro, buffer dove abbiamo scritto il mess, lunghezza buff tipo
@@ -22,6 +26,10 @@ int main(void) {
         sleep(1);
         execve("/usr/bin/ls", arg, NULL); //cerca path, argomenti = il comando da usare , null
         //tutto quello che c'è dopo non viene eseguito, a meno che non fallisce
+
+        //getpid() per ottenere il pid (del figlio)
+        //getppid() per ottenere il pid (del padre)
+        
     }else{
         err_sys("Fork error");
     }
